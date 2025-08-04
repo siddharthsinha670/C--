d631492abe95bbb0784f32a6c0a61ef6f97896ca
siddharthsinha670 <siddharthsinha670@gmail.com>
2025-08-04 21:47:33 +0530

    Continue Statement

---
+++ continue01.cpp
@@ -0,0 +1,16 @@
+//continue program
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+
+    for(i=1;i<=10;i++)
+    {
+        if(i==5)
+        continue; // continue- it means if the condition satisfys it will remove this condition and then continue
+        cout<<i<<ends;
+    }
+    return 0;
+}
