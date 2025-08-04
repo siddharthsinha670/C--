5975aef184988a19ba127f2b6b22d108373e1447
siddharthsinha670 <siddharthsinha670@gmail.com>
2025-08-04 21:47:50 +0530

    Exit Statement

---
+++ exit01.cpp
@@ -0,0 +1,13 @@
+//exit statement program;
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"c programming"<<endl;
+    cout<<"c++ programming"<<endl;
+    exit(0); //it will end the program here.
+     cout<<"java"<<endl;
+    cout<<"python";
+    return 0;
+}
