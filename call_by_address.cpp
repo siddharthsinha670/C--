712fe0f19227bbf2f3444052070ebe44759ebdb4
siddharthsinha670 <siddharthsinha670@gmail.com>
2025-08-20 21:33:08 +0530

    Call By Address
    Introduces call_by_address.cpp demonstrating swapping two integers using pointers. Includes executable call_by_address.exe for running the example.

---
+++ call_by_address.cpp
@@ -0,0 +1,27 @@
+//call by address
+
+
+#include<iostream>
+using namespace std;
+void swap(int *a,int *b)
+{
+    cout<<endl<<"value of the a="<<a;
+    cout<<endl<<"value of the b="<<b;
+    int c=*a;
+    *a=*b;
+    *b=c;
+}
+int main()
+{
+    int x,y;
+    cout<<"Enter the two number:";
+    cin>>x>>y;
+    cout<<endl<<"value of the x:"<<x;
+    cout<<endl<<"value of the y:"<<y;
+    cout<<endl<<"value of the address:"<<&x;
+    cout<<endl<<"value of the address:"<<&y;
+    swap(&x,&y);
+    cout<<endl<<"value of the x="<<x;
+    cout<<endl<<"value of the y="<<y;
+    return 0; 
+}
