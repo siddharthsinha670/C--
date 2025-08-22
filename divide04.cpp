70671efcca08601c686684a4c65ec80dc7eaee1e
siddharthsinha670 <siddharthsinha670@gmail.com>
2025-08-22 21:31:17 +0530

    Question Practice

---
+++ divide04.cpp
@@ -0,0 +1,14 @@
+//Question Practice
+//take two number and print their division(floating)
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    float a,b,c;
+    cout<<"Enter the number:";
+    cin>>a>>b;
+    c=b/a;
+    cout<<("The division of the number is:")<<c;
+    return 0;
+}
---
+++ divide05.cpp
@@ -0,0 +1,14 @@
+//practice question
+//Take two numbers and print their quotient 
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    float a,b,c;
+    cout<<"Enter the number:";
+    cin>>a>>b;
+    c=a/b;
+    cout<<"the quotient of the number is:"<<c;
+    return 0;
+}
