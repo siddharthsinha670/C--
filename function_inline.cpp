4d1ba0d2f2063bbc38e2ba7cff8155abf035b480
siddharthsinha670 <siddharthsinha670@gmail.com>
2025-08-27 20:56:08 +0530

    inline swap function
    Introduces function_inline.cpp demonstrating the use of an inline swap function with user input and output.

---
+++ function_inline.cpp
@@ -0,0 +1,22 @@
+//inline statement
+#include<iostream>
+using namespace std;
+inline void swap(int &a,int &b)
+{
+    int c=a;
+    a=b;
+    b=c;
+}
+int main()
+{
+    int x,y;
+    cout<<"Enter the number:";
+    cin>>x>>y;
+    cout<<endl<<"value of the x="<<x;
+    cout<<endl<<"value of the y="<<y;
+    swap(x,y);
+    cout<<endl<<"value of x="<<x;
+    cout<<endl<<"value of the y="<<y;
+    return 0;
+
+}
