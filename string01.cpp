1cd40bf450807f50a981c0804a141c6c3749b46b
siddharthsinha670 <siddharthsinha670@gmail.com>
2025-10-01 10:38:13 +0530

    STRING

---
+++ string01.cpp
@@ -0,0 +1,14 @@
+//wap to print your name and cout the letter
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+    char str[]="Siddharth";
+    cout<<str<<endl;
+    int r=strlen(str);
+    cout<<r;
+    return 0;
+
+}
+    
---
+++ string02.cpp
@@ -0,0 +1,15 @@
+//Wap print your name and count and display reverese of it
+
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+    char str[]="siddharth";
+    char str2[]="sinha";
+    cout<<str<<str2<<endl;
+    strrev(str);
+    //strrev(str2);
+    cout<<str<<str2;
+    return 0;
+}
