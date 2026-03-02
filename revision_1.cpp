e6b4838e1663356c9b8ce31b52841729b99a72bb
SIDDHARTH SINHA <siddharthsinha670@gmail.com>
2026-03-02 14:14:39 +0530

    revision

---
+++ revision_1.cpp
@@ -0,0 +1,8 @@
+//revision 1
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"hello world";
+    return 0;   
+}
---
+++ revision_2.cpp
@@ -0,0 +1,10 @@
+//revision_2
+//Check Odd and Even
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<"Enter the number:">
+    cin>>a>>b;
+}
